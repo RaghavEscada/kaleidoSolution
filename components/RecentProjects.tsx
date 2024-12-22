bb79559651f6734637d57c9992021c73c8d9341f
RaghavJetski <155087420+RaghavEscada@users.noreply.github.com>
2024-12-22 22:33:24 +0530

    .

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -30,7 +30,7 @@ const RecentProjects = () => {
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                   style={{ backgroundColor: "#13162D" }}
                 >
-                  <img src="" alt="bgimg" className="w-full h-full object-cover" />
+                  <img src="" className="w-full h-full object-cover" />
                 </div>
                 <img
                   src={item.img}
